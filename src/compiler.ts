6487c576a4f27ff9028c7b7de912b947994aac06
Art Deineka <art.deineka@darkruby.co.uk>
2019-03-29 20:43:46 +0000

    compiler fixes

--- src/compiler.ts
+++ src/compiler.ts
@@ -14,13 +14,13 @@ import {
 } from "./parser";
 
 const allButLast = <T>(xs: T[]): T[] => {
-  const [, ...ys] = xs.reverse();
-  return ys.reverse();
+  const [, ...ys] = [...xs].reverse();
+  return [...ys].reverse();
 };
 
 const last = <T extends Ast>(xs: T[]): null | T => {
   if (xs.length) {
-    const [x] = xs.reverse();
+    const [x] = [...xs].reverse();
     return x;
   }
   return null;
@@ -29,33 +29,45 @@ const last = <T extends Ast>(xs: T[]): null | T => {
 const compileNum = (ast: AstNum) => `${ast.value}`;
 const compileStr = (ast: AstStr) => `"${ast.value}"`;
 const compileBool = (ast: AstBool) => (ast.value ? "true" : "false");
-const compileVar = (ast: AstVar) => `var ${ast.value}`;
-const compileProg = (ast: AstProg) => {
-  if (ast.body.length === 1) {
-    return compile(ast.body[0]);
+const compileVar = (ast: AstVar) => `${ast.value}`;
+const compileLastInBlock = (ast: Ast | null): string => {
+  if (ast === null) return "";
+  if (ast.type == AstNode.Assign) {
+    return `${compile(ast)}\n ${compileLastInBlock(ast.left)}`;
+  } else {
+    return `return ${compile(ast)};`;
   }
-  const lastAst = last(ast.body);
-  if (lastAst) {
-    return `{\n${allButLast(ast.body)
-      .map(compile)
-      .join(";\n")} return ${compile(lastAst)}}\n`;
-  }
-  return "";
 };
+const compileSelfInvokingFunc = (ast: AstProg) => {
+  if (ast.body.length === 1) {
+    return compileLastInBlock(ast.body[0]);
+  }
+  const bodyAst = allButLast(ast.body);
+  const lastAst = last(ast.body);
+  return `(function(){
+${bodyAst.map(ast => `${compile(ast)};\n`).join("")}${compileLastInBlock(
+    lastAst
+  )}
+}())`;
+};
+
 const compileAssign = (ast: AstAssign) =>
-  `${compile(ast.left)} = ${compile(ast.right)}\n`;
+  ast.declare
+    ? `var ${compile(ast.left)} = ${compile(ast.right)}`
+    : `${compile(ast.left)} = ${compile(ast.right)}`;
+
 const compileBinary = (ast: AstBinary) =>
-  `${compile(ast.left)} ${ast.op} ${compile(ast.right)}\n`;
+  `${compile(ast.left)} ${ast.op} ${compile(ast.right)}`;
 const compileIf = (ast: AstIf) =>
   `(${compile(ast.cond)}) ? ${compile(ast.then)} : ${compile(ast.else)}`;
 const compileLambda = (ast: AstLambda) =>
-  `function (${ast.vars.map(compileVar).join(",")}) {\n${compileLambdaBody(
+  `function (${ast.vars.map(compileVar).join(",")}) {${compileLambdaBody(
     ast.body
-  )}\n}`;
+  )}}`;
 const compileLambdaBody = (ast: Ast) =>
-  ast.type === AstNode.Prog ? compile(ast) : `return ${compile(ast)};\n`;
+  ast.type === AstNode.Prog ? compile(ast) : `return ${compile(ast)};`;
 const compileCall = (ast: AstCall) =>
-  `(${compile(ast.func)})(${ast.args.map(compile).join(",")})`;
+  `${compile(ast.func)}(${ast.args.map(compile).join(",")})`;
 
 export const compile = (ast: Ast): string => {
   switch (ast.type) {
@@ -68,7 +80,7 @@ export const compile = (ast: Ast): string => {
     case AstNode.Var:
       return compileVar(ast);
     case AstNode.Prog:
-      return compileProg(ast);
+      return compileSelfInvokingFunc(ast);
     case AstNode.Assign:
       return compileAssign(ast);
     case AstNode.Binary:
